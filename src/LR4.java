7332336c8dca94e5ecf7ee8a4388613c4688a4f5
YHT97 <91371115+YHT97@users.noreply.github.com>
2022-05-15 23:35:28 +0300

    beta

--- src/LR4.java
+++ src/LR4.java
@@ -4,19 +4,19 @@ import java.util.Random;
 
 public class LR4 {
     public static void main(String[] args) {
-        Node<Data> tree = new Node<>(new Data());
+        Node<Integer> tree = new Node<>((int) (Math.random() * 100) -100);
         for (int i = 0;i<3;i++){
-            tree.addChild(new Node<>(new Data()));
+            tree.addChild(new Node<>((int) (Math.random() * 100) -100));
         }
-        for(Node<Data> child : tree.getChildren()){
+        for(Node<Integer> child : tree.getChildren()){
             for(int i =0;i<3;i++){
-                child.addChild(new Node<>(new Data()));
+                child.addChild(new Node<>((int) (Math.random() * 100) -100));
             }
         }
-        printTree(tree,"  ");
+        printTree(tree,"*");
         tree.remove(tree.getChildren().get(0).getChildren().get(0));
         System.out.println("-----------------------------------------------------------------");
-        printTree(tree,"  ");
+        printTree(tree,"*");
 
 
 
@@ -26,34 +26,6 @@ public class LR4 {
         node.getChildren().forEach(each -> printTree(each, appender + appender));
     }
 
-    static class Data {
-        public int[] index;
-        public int data;
-        public String str;
-
-        public Data() {
-            this.index = getArray();
-            this.data = (int)(Math.random()*200-100);
-            this.str = RandString();
-        }
-    }
-
-    static int[] getArray() {
-        int[] array = new int[3];
-        for (int i = 0; i < 3; i++) {
-            array[i] = (int) (Math.random() * 200) - 100;
-        }
-        return array;
-    }
-    public static String RandString(){
-        String alp = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        Random rand = new Random();
-        StringBuilder build = new StringBuilder();
-        for(int i=0;i<10;i++){
-            build.append(alp.charAt(rand.nextInt(alp.length())));
-        }
-        return build.toString();
-    }
     public static class Node<T> {
 
         private T data = null;
